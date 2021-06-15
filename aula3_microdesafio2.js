0e30f250cf4c11b53774e0089ecd6fc8b1128fb8
Murilo <muca.murilo@hotmail.com>
2021-06-15 12:29:04 -0300

    Atividades

---
+++ aula3_microdesafio2.js
@@ -0,0 +1,9 @@
+let soma = (n1, n2) => n1+n2
+let subt = (n1, n2) => n1-n2
+let mult = (n1, n2) => n1*n2
+let div = (n1, n2) => (n1/n2).toFixed(2)
+
+console.log (soma(1,5))
+console.log (subt(1,5))
+console.log (mult(1,5))
+console.log (div(1,5))
---
+++ aula4_microdesafio.js
@@ -0,0 +1,14 @@
+let estacaodoAno = "verao";
+
+switch (estacaodoAno){
+    case "verao":
+        console.log(“Que calor hein?!”)
+        break;
+    case "inverno":
+        console.log(“Frio dms!”)
+        break;
+    case "outono":
+        console.log(“É a estação em que as folhas caem.”)
+        break;
+    case ""
+}
